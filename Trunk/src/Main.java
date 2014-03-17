6ff1dd46ad92fed51615d1cd3b8fec3104cb8f93
AlexNiki <av.nikiforov@gmail.com>
2014-03-18 01:11:26 +0600

    second commit
    Clear Main

--- Trunk/src/Main.java
+++ Trunk/src/Main.java
@@ -3,17 +3,9 @@
  * Date: 09.03.14
  * Time: 20:30
  */
-import java.io.*;
 
 public class Main {
 
    public static void main(String[] args){
-       CurrencyRate currencyRate = CurrencyRate.getInstance();
-       try{
-            currencyRate.UpdateCurrencyRate();
-       }catch (IOException ex)
-       {
-        ex.printStackTrace();
-       }
    }
 }
