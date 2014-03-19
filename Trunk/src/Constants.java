f2e71340da5227b0c73d18fef689619fee56bda3
AlexNiki <Av.nikiforov@gmail.com>
2014-03-19 16:36:35 +0500

    Custom exception

--- Trunk/src/Constants.java
+++ Trunk/src/Constants.java
@@ -15,4 +15,32 @@ public interface Constants {
     public static final String CUR_NOM              = "Vnom";
     public static final String CUR_CURS             = "Vcurs";
     public static final String CUR_CODE             = "Vcode";
+
+
+    public enum ConversionRateError {
+        ERROR_FORMING_SOAP(0, "Ошибка формирования SOAP запроса."),
+        INCORRECT_RESPONSE(1, "Некорректный ответ сервера ЦБ РФ."),
+        ERROR_CONNECT(2,"Не удалось обратиться к серверу ЦБ РФ.");
+
+        private final int code;
+        private final String description;
+
+        private ConversionRateError(int code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            return code + ": " + description;
+        }
+    }
 }
